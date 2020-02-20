27ff4e6bb027cf932fb8df70e6932f2990dcde77
vkshay3400 <akshayp2208@gmail.com>
2020-02-20 09:00:35 +0530

    Added: Multi flip coin singlet

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,13 +2,52 @@
 
 echo "*************************Flip Coin*************************"
 
-# GET RANDOM VALUES
-randomFlipCoin=$((RANDOM%2))
+# CONSTANT
+ISHEAD=1
+SINGLET=1
 
-# CHECK WEHTER HEADS OR TAIL
-if [ $randomFlipCoin -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+# DICTIONARY
+declare -A coinFlip
+
+# INPUT FROM USER
+read -p "Enter a number to multi-time flip a coin: " flipcoin
+
+# GET RANDOM VALUES
+function isflip(){
+local NumberCoin=$1
+for (( flip=0; flip<$flipcoin; flip++ ))
+do
+	for (( coin=0; coin<$NumberCoin; coin++ ))
+	do
+		randomFlipCoin=$((RANDOM%2))
+
+		# CHECK WEHTER HEADS OR TAIL
+		if [ $randomFlipCoin -eq $ISHEAD ]
+		then
+			coinSide+=H
+		else
+			coinSide+=T
+		fi
+	done
+
+	# STORING IN DICTIONARY
+	((coinFlip[$coinSide]++))
+	coinSide=""
+done
+echo "FlipCoin value :" ${coinFlip[@]}
+echo "FlipCoin key : "${!coinFlip[@]}
+}
+
+# FUNCTION FOR PERCENTAGE
+function getPercentage(){
+for index in ${!coinFlip[@]}
+do
+	coinFlip[$index]=`echo "scale=2 ; ${coinFlip[$index]}*100/$flipcoin" | bc`%
+done
+	echo "FlipCoin in percentage: "${coinFlip[@]}
+}
+
+# PRINT PERCENTAGE
+isflip $SINGLET
+getPercentage $SINGLET
+
